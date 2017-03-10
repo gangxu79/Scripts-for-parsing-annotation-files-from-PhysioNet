8f3c044713409b6481d1a070d7e2034184fa6d30
Gang <gangxu79@gmail.com>
2017-03-10 16:10:26 -0500

    Update read_ucddb_annotation.R

--- UCD Sleep Apnea Database/read_ucddb_annotation.R
+++ UCD Sleep Apnea Database/read_ucddb_annotation.R
@@ -1,12 +1,21 @@
 library(lubridate)
 library(dplyr)
 library(tools)
+library(xlsx)
 
-setwd('./')
+setwd(Sys.getenv("HOME"))
 rm(list = ls())
+# read record start time from subject detail spreadsheet file
+filename <- './SubjectDetails.xls'
+detail <- read.xlsx(filename, sheetIndex = 1, stringsAsFactors = FALSE)
+
 # read annotation text file from PhysioNet 
 # St. Vincent's University Hospital / University College Dublin Sleep Apnea Database
-filename <- 'https://physionet.org/physiobank/database/ucddb/ucddb006_respevt.txt'
+filename <- 'https://physionet.org/physiobank/database/ucddb/ucddb002_respevt.txt'
+subjectid <- basename(filename)
+subjectid <- unlist(strsplit(subjectid, split = '_'))
+subjectid <- subjectid[1]
+
 ann <- read.fwf(filename,
                 header = FALSE,
                 skip = 3,
@@ -24,8 +33,20 @@ ann <- read.fwf(filename,
                               'B/T Rate',
                               'B/T Change'),
                 widths = c(10, 10, 5, 5, 11, 7, 6, 6, 10, 8))
+# compensate from beginning
+subidx <- match(subjectid, tolower(detail$Study.Number))
 # convert HH:MM:SS to seconds
-ann$TimeInSec <- as.integer(seconds(hms(ann$Time)))
+at <- as.integer(seconds(hms(ann$Time)))
+st <- as.integer(seconds(hms(detail$PSG.Start.Time[subidx])))
+timelength <- at - st
+for (i in 1:length(timelength))
+{
+    if (timelength[i] < 0)
+    {
+      timelength[i] <- timelength[i] + 3600*24
+    }
+}
+ann$TimeInSec <- timelength
 # set factor variables
 ann$Type <- as.factor(ann$Type)
 ann$Snore <- factor(ann$Snore, 
